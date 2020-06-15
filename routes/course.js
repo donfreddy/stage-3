f75b3f087727ad00633ae1e20b1f805f342e5adf
Freddy50 <Freddy>
2020-06-15 11:32:41 +0100

    first commit

--- routes/course.js
+++ routes/course.js
@@ -2,6 +2,36 @@ const courseRoutes = (app, fs) => {
 
   const dataPath = "./data/courses.json";
 
+  const readFile = (
+    callback,
+    returnJson = false,
+    filePath = dataPath,
+    encoding = "utf8"
+  ) => {
+    fs.readFile(filePath, encoding, (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      callback(returnJson ? JSON.parse(data) : data);
+    });
+  };
+
+  const writeFile = (
+    fileData,
+    callback,
+    filePath = dataPath,
+    encoding = "utf8"
+  ) => {
+    fs.writeFile(filePath, fileData, encoding, (err) => {
+      if (err) {
+        throw err;
+      }
+
+      callback();
+    });
+  };
+
 
   // CREATE
   app.post("/courses", (req, res) => {
--- routes/student.js
+++ routes/student.js
@@ -2,6 +2,36 @@ const studentRoutes = (app, fs) => {
 
   const dataPath = "./data/students.json";
 
+  const readFile = (
+    callback,
+    returnJson = false,
+    filePath = dataPath,
+    encoding = "utf8"
+  ) => {
+    fs.readFile(filePath, encoding, (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      callback(returnJson ? JSON.parse(data) : data);
+    });
+  };
+
+  const writeFile = (
+    fileData,
+    callback,
+    filePath = dataPath,
+    encoding = "utf8"
+  ) => {
+    fs.writeFile(filePath, fileData, encoding, (err) => {
+      if (err) {
+        throw err;
+      }
+
+      callback();
+    });
+  };
+
 
   // CREATE
   app.post("/students", (req, res) => {
